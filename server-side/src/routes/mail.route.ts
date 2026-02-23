8c52a0fadfd8c1d2d0698358c1ffda344237c020
Vulprit Prooze <raileyalin@gmail.com>
2026-02-23 15:21:22 +0800

    fix: removed mail route

--- server-side/src/routes/mail.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { sendCertParticipationController } from "../controllers/mail.controller";
-
-const router = Router()
-
-// Add mail routes here
-
-export default router
